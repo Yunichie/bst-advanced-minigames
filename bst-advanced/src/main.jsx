cc7369963effd999286799724080f0514caa47f8
Yunichie <alfarelsandriano@gmail.com>
2024-09-20 08:55:01 +0700

    no change

--- bst-advanced/src/main.jsx
+++ bst-advanced/src/main.jsx
@@ -1,6 +1,6 @@
 import React, { StrictMode } from 'react'
-import ReactDOM, { createRoot } from 'react-dom/client'
-import {BrowserRouter, createBrowserRouter, Route, RouterProvider, Routes} from 'react-router-dom'
+import { createRoot } from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import Hangman from './games/Hangman.jsx'
